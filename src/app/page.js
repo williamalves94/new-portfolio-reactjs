bb2e715e90b29df57cb4c8f687d77f296d54feca
William Alves <williamcostacardoso94@gmail.com>
2024-11-17 14:13:53 -0300

    feat: adicionando componentes na home page

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,12 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-72 bg-zinc-950 p-4">sidebar</aside>
-        <main>main</main>
-        <footer>footer</footer>
+        <aside className="w-60 bg-zinc-950 p-4 text-white">sidebar</aside>
+        <main className="flex-1">main</main>
       </div>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-3">
+        footer
+      </footer>
     </div>
   );
 }
