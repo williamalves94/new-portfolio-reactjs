b562e57d8c44d9e3fa03483f01370302090890ad
William Alves <williamcostacardoso94@gmail.com>
2025-03-24 12:48:13 -0300

    feat: estilizando Header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,6 @@
 export default function Header({ title }) {
   return (
-    <header className="p-6 bg-zinc-700 text-white text-3xl">
+    <header className="shadow-md p-6 bg-zinc-700 text-white text-3xl">
       <h1>{title}</h1>
     </header>
   );
