d7531e9be12d021b89cb1e12e77ba29c227c7150
William Alves <williamcostacardoso94@gmail.com>
2025-03-24 13:12:38 -0300

    chore: testando o erro

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Layout from "@/components/layout/layout";
+import Layout from "@/components/layout/Layout";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
